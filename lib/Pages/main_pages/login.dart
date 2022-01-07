d805d1d04e4b9204e8173bab02fdccdaa9333574
Tarun <84436181+code-simp@users.noreply.github.com>
2022-01-07 13:00:15 +0530

    added signup and login functionalities

--- lib/Pages/main_pages/login.dart
+++ lib/Pages/main_pages/login.dart
@@ -3,24 +3,22 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-class onboard_3 extends StatefulWidget {
-  const onboard_3({Key? key}) : super(key: key);
+class login extends StatefulWidget {
+  const login({Key? key}) : super(key: key);
 
   @override
-  _onboard_3State createState() => _onboard_3State();
+  _loginState createState() => _loginState();
 }
 
-class _onboard_3State extends State<onboard_3> {
+class _loginState extends State<login> {
 
-  Future<http.Response> signUp(String name, String email, String mobile_no, String password) async {
-    return http.post(Uri.parse('http://10.0.2.2:3001/users/signup'),
-        headers: <String, String>{
+  Future<http.Response> login(String email, String password) async {
+    return http.post(Uri.parse('http://10.0.2.2:3001/users/login'),
+      headers: <String, String>{
         'Content-Type': 'application/json; charset=UTF-8',
-        },
+      },
       body: json.encode(<String, String>{
-        'name': name,
         'email': email,
-        'mobile_no': mobile_no,
         'password': password
       }),
     );
@@ -31,9 +29,7 @@ class _onboard_3State extends State<onboard_3> {
   @override
   Widget build(BuildContext context) {
 
-    final nameCont = TextEditingController();
     final emailCont = TextEditingController();
-    final phoneNoCont = TextEditingController();
     final passwordCont = TextEditingController();
 
     return SafeArea(
@@ -69,7 +65,7 @@ class _onboard_3State extends State<onboard_3> {
                             fit: BoxFit.fill
                         )
                     ),
-                    height: 600,
+                    height: 500,
                     width: 350,
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.center,
@@ -77,7 +73,7 @@ class _onboard_3State extends State<onboard_3> {
                       children: [
                         Column(
                           children: [
-                            Text('One last\nstep.',
+                            Text('Login',
                               style: TextStyle(
                                 fontSize: 36,
                                 fontFamily: 'poppins',
@@ -85,7 +81,7 @@ class _onboard_3State extends State<onboard_3> {
                               ),
                               textAlign: TextAlign.center,),
                             SizedBox(height: 10,),
-                            Text('Fill these essential details\nandyou’re good to go',
+                            Text('Fill these essential details\nto login to your account',
                               style: TextStyle(
                                 fontSize: 18,
                                 fontFamily: 'poppins',
@@ -98,51 +94,23 @@ class _onboard_3State extends State<onboard_3> {
                                 // _buildNameField(),
                                 // _buildEmailField(),
                                 // _buildPhone_noField(),
-                                SizedBox(height: 0,),
+                                SizedBox(height: 20,),
 
-                              Padding(
-                                padding: const EdgeInsets.fromLTRB(40.0,10,40,0),
-                                child: TextFormField(
-                                  controller: nameCont,
-                                  decoration: InputDecoration(labelText: 'Name',
-                                    border: OutlineInputBorder(),
-                                    fillColor: Color.fromRGBO(255, 248, 208, 1),
-                                    filled: true,
-                                    enabledBorder: InputBorder.none,
 
+                                Padding(
+                                  padding: const EdgeInsets.fromLTRB(40.0,10,40,0),
+                                  child: TextFormField(
+                                    controller: emailCont,
+                                    decoration: InputDecoration(labelText: 'Email',
+                                      border: OutlineInputBorder(),
+                                      fillColor: Color.fromRGBO(255, 248, 208, 1),
+                                      filled: true,
+                                      enabledBorder: InputBorder.none,
+
+                                    ),
                                   ),
                                 ),
-                              ),
 
-
-                              Padding(
-                                padding: const EdgeInsets.fromLTRB(40.0,10,40,0),
-                                child: TextFormField(
-                                  controller: emailCont,
-                                  decoration: InputDecoration(labelText: 'Email',
-                                    border: OutlineInputBorder(),
-                                    fillColor: Color.fromRGBO(255, 248, 208, 1),
-                                    filled: true,
-                                    enabledBorder: InputBorder.none,
-
-                                  ),
-                                ),
-                              ),
-
-                              Padding(
-                                padding: const EdgeInsets.fromLTRB(40.0,10,40,0),
-                                child: TextFormField(
-                                  controller: phoneNoCont,
-                                  decoration: InputDecoration(labelText: 'Phone no',
-                                    border: OutlineInputBorder(),
-                                    fillColor: Color.fromRGBO(255, 248, 208, 1),
-                                    filled: true,
-                                    enabledBorder: InputBorder.none,
-
-                                  ),
-
-                                ),
-                              ),
 
                                 Padding(
                                   padding: const EdgeInsets.fromLTRB(40.0,10,40,0),
@@ -160,19 +128,11 @@ class _onboard_3State extends State<onboard_3> {
                                 ),
                                 SizedBox(height: 20,),
 
-                                Text('By clicking the following button you\naccept the terms and conditions',
-                                  style: TextStyle(
-                                    fontSize: 14,
-                                    fontFamily: 'poppins',
-                                    fontWeight: FontWeight.w500,
-                                  ),
-                                  textAlign: TextAlign.center,
-                                ),
                                 SizedBox(height: 20,),
                                 RaisedButton(
                                   child: Padding(
                                     padding: const EdgeInsets.fromLTRB(95,10,95,10),
-                                    child: Text('Lets go',
+                                    child: Text('Login',
                                       style: TextStyle(
                                         fontSize: 18,
                                         fontFamily: 'poppins',
@@ -184,15 +144,13 @@ class _onboard_3State extends State<onboard_3> {
 
                                   onPressed: () async {
 
-                                    String NameF = nameCont.text;
                                     String emailF = emailCont.text;
-                                    String phoneNoF = phoneNoCont.text;
                                     String passwordF = passwordCont.text;
 
-                                    http.Response response = await signUp(NameF, emailF, phoneNoF, passwordF);
+                                    http.Response response = await login(emailF, passwordF);
 
                                     Map data = json.decode(response.body);
-                                    String currentUser_id = data['_id'];
+                                    print(data);
 
                                   },
                                 )
--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:airway_flutter/Pages/User_onboard2.dart';
 import 'package:airway_flutter/Pages/onboard_3.dart';
 import 'package:airway_flutter/Pages/user_onboardmain.dart';
 import 'package:airway_flutter/Pages/User_onboard3.dart';
+import 'package:airway_flutter/Pages/main_pages/login.dart';
 import 'package:airway_flutter/Pages/Edit_profile.dart';
 import 'package:airway_flutter/pages/main_pages/account_page.dart';
 import 'package:airway_flutter/pages/main_pages/points_history.dart';
@@ -16,12 +17,13 @@ import 'package:airway_flutter/Pages/main_pages/booking_confirm.dart';
 
 
 void main() => runApp(MaterialApp(
-  initialRoute: 'onboard3',
+  initialRoute: 'login',
   routes: {
    // '/': (context) => loading(),
     'user_onboard1':(context) => user_onboard1(),
     'user_onboard2':(context) => user_onboard2(),
     'user_onboard3':(context) => user_onboard3(),
+    'login':(context) => login(),
     'onboard3':(context) => onboard_3(),
     'user_onboardmain':(context) => user_onboardmain(),
     'user_account' : (context) => Account(),
