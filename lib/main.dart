650ee27eca6e614476018998b8b8bc5e010acb1f
Harish V <harishhdvp1623@gmail.com>
2022-01-20 22:13:31 +0530

    dark mode
    faq page changed design

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ import 'package:airway_flutter/components/Transaction_Card.dart';
 import 'package:airway_flutter/components/Transactions_Builder.dart';
 
 void main() => runApp(MaterialApp(
-  initialRoute: 'user_account',
+  initialRoute: 'faq',
 
   routes: {
    // '/': (context) => loading(),
